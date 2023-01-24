6f7908012440ae928ef766d2932b2abcbce2a097
May Hemade <71593691+May-Hemade@users.noreply.github.com>
2023-01-24 14:28:07 +0200

    fixed

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -7,30 +7,20 @@ export function CommentArea(props) {
   const [comments, setComments] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
-  const [prevProps, setPrevProps] = useState(props)
-
-  setError(error)
-
-  setLoading(loading)
-
-  setComments(comments)
 
   const getComments = () => {
-    console.log(this.props.id)
-    if (!this.props.id) {
+    console.log(props.id)
+    if (!props.id) {
       return
     }
-    this.setLoading(true)
-    this.setError(false)
-    fetch(
-      `https://striveschool-api.herokuapp.com/api/comments/${this.props.id}`,
-      {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2NlODBhYjM4YzkzNjAwMTUwNTc3YWIiLCJpYXQiOjE2NzQ0Nzc3NDAsImV4cCI6MTY3NTY4NzM0MH0._v7xRF3NJfqzvuSW8-D5vFdj_9fiSq17u4mBui7XFbk",
-        },
-      }
-    )
+    setLoading(true)
+    setError(false)
+    fetch(`https://striveschool-api.herokuapp.com/api/comments/${props.id}`, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2NlODBhYjM4YzkzNjAwMTUwNTc3YWIiLCJpYXQiOjE2NzQ0Nzc3NDAsImV4cCI6MTY3NTY4NzM0MH0._v7xRF3NJfqzvuSW8-D5vFdj_9fiSq17u4mBui7XFbk",
+      },
+    })
       .then((response) => response.json())
       .then((comments) => {
         console.log(comments)
@@ -44,15 +34,13 @@ export function CommentArea(props) {
         console.log(error)
       })
   }
-  useEffect(() => {
-    getComments()
-  }, [])
+  // useEffect(() => {
+  //   getComments()
+  // }, [])
 
   useEffect(() => {
-    if (prevProps.id !== props.id) {
-      getComments()
-    }
-  }, [props])
+    getComments()
+  }, [props.id])
 
   return (
     <div className="comment-area">
